14259aabc34c91efa1592fb0b9f9a809d4bf58d7
xmcgcg <xmcgcg@qq.com>
2015-05-28 16:23:10 +0800

    Source file for the derived Ma class

--- chess02_Ma.cpp
+++ chess02_Ma.cpp
@@ -4,7 +4,7 @@ using std::abs;
 
 Ma::Ma(SC x, SC y, const string& nm, color cl): Chess(x, y, nm, cl) {} //use Chess's constructor
 
-bool Ma::move(SC x, SC y, vector<Chess*> ar) //return TRUE if the movement is valid, otherwise return FALSE
+bool Ma::move(SC x, SC y, vector<Chess*> ar) //return TRUE if moving to (x,y) is valid, otherwise return FALSE
 {
 	int Xdif = abs(x - Xcoord); //Xdif represents the number of columns to the destination
 	int Ydif = abs(y - Ycoord); //Ydif represents the number of rows to the destination
