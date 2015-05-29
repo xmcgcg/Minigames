ddb43e49b3f405b3ce059c01b9ff68730f4b3c98
xmcgcg <xmcgcg@qq.com>
2015-05-29 15:27:20 +0800

    Source file for the derived Shi class

--- chess04_Shi.cpp
+++ chess04_Shi.cpp
@@ -2,28 +2,28 @@
 
 Shi::Shi(SC x, SC y, const string& nm, color cl): Chess(x, y, nm, cl) {} //use Chess's constructor
 
-bool Shi::move(SC x, SC y, vector<Chess*> ar) //return TRUE if moving to (x,y) is valid, otherwise return FALSE
+bool Shi::move(SC x, SC y, vector<Chess*> ar)   //return TRUE if moving to (x,y) is valid, otherwise return FALSE
 {
 	switch (Ycoord)
 	{
-	case 'a':                      //if 仕/士 is on the first or third row
+	case 'a':                               //if 仕/士 is on the first or third row
 	case 'c':
 		if (x == '5' && y == 'b')
-			return true;   //it can only move to B5 to return TRUE
+			return true;            //it can only move to B5 to return TRUE
 		else
 			return false;
-	case 'b':                      //if 仕/士 is at B5
+	case 'b':                               //if 仕/士 is at B5
 		if ((x == '4' || x == '6') && (y == 'a' || y == 'c'))
-			return true;   //it can move to A4/A6/B4/B6 to return TRUE
+			return true;            //it can move to A4/A6/B4/B6 to return TRUE
 		else
 			return false;
-	case 'h':                      //if 仕/士 is on the last ot third last row
+	case 'h':                               //if 仕/士 is on the last ot third last row
 	case 'j':
 		if (x == '5' && y == 'i')
-			return true;   //it can only move to I5 to return true
+			return true;            //it can only move to I5 to return true
 		else
 			return false;
-	case 'i':                      //if 仕/士 is at I5
+	case 'i':                               //if 仕/士 is at I5
 		if ((x == '4' && x == '6') && (y == 'h' || y == 'j'))
 			return true;            //it can move to H4/H6/I4/I6  to return TRUE
 		else
