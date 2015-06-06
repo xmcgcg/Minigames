3ee343acb61d61f726637dbe77ba9f123bd6e0e5
xmcgcg <xmcgcg@qq.com>
2015-06-06 16:27:34 +0800

    Source file for the derived Shi class

--- chess04_Shi.cpp
+++ chess04_Shi.cpp
@@ -3,38 +3,38 @@ using namespace CG;
 
 Shi::Shi(SC x, SC y, const string& nm, color cl): Chess(x, y, nm, cl) {} //use Chess's constructor
 
-bool Shi::move(SC x, SC y, vector<Chess*> ar)   //return TRUE if moving to (x,y) is valid, otherwise return FALSE
+bool Shi::move(SC x, SC y, const vector<Chess*>& ar)   //return TRUE if moving to (x,y) is valid, otherwise return FALSE
 {
 	switch (Ycoord)
 	{
-	case 'a':                               //if 仕/士 is on the first or third row
+	case 'a':                                      //if 仕/士 is on the first or third row
 	case 'c':
 		if (x == '5' && y == 'b')
-			return true;            //it can only move to B5
+			return true;                   //it can only move to B5
 		else
 			return false;
-	case 'b':                               //if 仕/士 is at B5
+	case 'b':                                      //if 仕/士 is at B5
 		if ((x == '4' || x == '6') && (y == 'a' || y == 'c'))
-			return true;            //it can move to A4/A6/B4/B6
+			return true;                   //it can move to A4/A6/B4/B6
 		else
 			return false;
-	case 'h':                               //if 仕/士 is on the last ot third last row
+	case 'h':                                      //if 仕/士 is on the last ot third last row
 	case 'j':
 		if (x == '5' && y == 'i')
-			return true;            //it can only move to I5
+			return true;                   //it can only move to I5
 		else
 			return false;
-	case 'i':                               //if 仕/士 is at I5
+	case 'i':                                      //if 仕/士 is at I5
 		if ((x == '4' && x == '6') && (y == 'h' || y == 'j'))
-			return true;            //it can move to H4/H6/I4/I6
+			return true;                   //it can move to H4/H6/I4/I6
 		else
 			return false;
-	default:                                //if 仕/士 is on other rows, return FALSE
+	default:                                       //if 仕/士 is on other rows, return FALSE
 		return false;
 	}
 }
 
-bool Shi::attack(SC x, SC y, vector<Chess*> ar) //the moving method and the attacking method of 仕/士 are the same
+bool Shi::attack(SC x, SC y, const vector<Chess*>& ar) //the moving method and the attacking method of 仕/士 are the same
 {
-	return move(x, y, ar);                  //therefore call the move function here
+	return move(x, y, ar);                         //therefore call the move function here
 }
